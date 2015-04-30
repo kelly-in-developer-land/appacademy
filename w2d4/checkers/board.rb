cf5f06c726f74b6367cc06b70988b557f9ba0d35
kelly-in-developer-land <kelly.witwickifaddegon@gmail.com>
2015-04-30 14:51:00 -0700

    Make functional move methods

--- w2d4/checkers/board.rb
+++ w2d4/checkers/board.rb
@@ -19,23 +19,52 @@ class Board
     @grid[row][col] = assignment
   end
 
+  def board_dup
+    dup = Board.new
+
+    piece_dup_proc = Proc.new do |piece|
+      dup[piece.position] = Piece.new(piece.color, self, [piece.position])
+    end
+
+    find_pieces(:red).each &piece_dup_proc
+    find_pieces(:yellow).each &piece_dup_proc
+
+    dup
+  end
+
   def display
     system 'clear'
+    puts ""
+    print "     0   1   2   3   4   5   6   7 \n"
     @grid.each_index do |row_idx|
+      print "#{row_idx}  "
       @grid[row_idx].each_index do |col_idx|
-        tile = @grid[row_idx][col_idx]
+        piece = @grid[row_idx][col_idx]
         tile_color = ((row_idx + col_idx).even? ? :black : :light_black)
-        unless tile.nil?
-          print " #{tile.symbol}  ".colorize(:color => tile.color, :mode => :bold, :background => tile_color)
+        unless piece.nil?
+          print " #{piece.symbol}  ".colorize(:color => piece.color, :mode => :bold, :background => tile_color)
         else
           print "    ".colorize(:background => tile_color)
         end
       end
-      print "\n"
+      print "  #{row_idx}"
+      puts ""
     end
+    print "     0   1   2   3   4   5   6   7 \n"
+    puts ""
   end
 
-  def update
+  def find_pieces(color)
+    @grid.flatten.compact.select { |piece| piece.color == color }
+  end
+
+  def update(piece, start_pos, end_pos)
+    self[end_pos] = piece
+    self[start_pos] = nil
+  end
+
+  def inspect
+    @piece
   end
 
 end
