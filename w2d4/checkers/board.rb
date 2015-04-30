b567aaa84636cf0aa662026f6752759145884011
kelly-in-developer-land <kelly.witwickifaddegon@gmail.com>
2015-04-30 11:07:37 -0700

    Initialize pieces, display board

--- w2d4/checkers/board.rb
+++ w2d4/checkers/board.rb
@@ -1,5 +1,41 @@
-# def initialize
-# end
+require_relative 'pieces'
+require 'colorize'
 
-# def update
-# end
+class Board
+
+  attr_reader :grid
+
+  def initialize
+    @grid = Array.new(8) { Array.new(8) }
+  end
+
+  def [](pos)
+    row, col = pos
+    @grid[row][col]
+  end
+
+  def []=(pos, assignment)
+    row, col = pos
+    @grid[row][col] = assignment
+  end
+
+  def display
+    system 'clear'
+    @grid.each_index do |row_idx|
+      @grid[row_idx].each_index do |col_idx|
+        tile = @grid[row_idx][col_idx]
+        tile_color = ((row_idx + col_idx).even? ? :black : :light_black)
+        unless tile.nil?
+          print " #{tile.symbol}  ".colorize(:color => tile.color, :mode => :bold, :background => tile_color)
+        else
+          print "    ".colorize(:background => tile_color)
+        end
+      end
+      print "\n"
+    end
+  end
+
+  def update
+  end
+
+end
--- w2d4/checkers/game.rb
+++ w2d4/checkers/game.rb
@@ -1,13 +1,36 @@
+require_relative 'board'
+require 'colorize'
 
+class Game
 
+  def initialize
+    @board = Board.new
+    populate(:red, 0)
+    populate(:yellow, 5)
+  end
 
+  def play
+    @board.display
+    # gameplay
+    # do getch stuff somewhere...
+  end
 
+  private
 
+    def populate(color, first_row_idx)
 
+      i = first_row_idx
+      until i == first_row_idx + 3
+        @board.grid[i].each_with_index do |tile, col_idx|
+            @board.grid[i][col_idx] = Piece.new(color, :pawn) if (i + col_idx).even?
+        end
+        i += 1
+      end
+    end
 
+end
 
-
-
-
-
-# run as a script
+if __FILE__ == $PROGRAM_NAME
+  game = Game.new
+  game.play
+end
