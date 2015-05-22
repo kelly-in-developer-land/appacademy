34f49ddde3d05a34232a0a8d55cf198f2df70d23
kelly-in-developer-land <kelly.witwickifaddegon@gmail.com>
2015-05-22 15:59:39 -0700

    Merge w5d4

---
+++ w5/w5d5/ttt/game.js
@@ -0,0 +1,6 @@
+// You'll want to require your ttt/board.js file.
+// Write the Game constructor such that it takes a reader interface as an argument.
+// As in the previous exercise, write a #run(completionCallback) method.
+
+// play method that loops, reading in user moves.
+// When the game is over, exit the loop.
