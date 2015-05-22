d5acbf8624e6ada7fcba7b19bb21b0eb25f414ce
kelly-in-developer-land <kelly.witwickifaddegon@gmail.com>
2015-05-21 17:57:54 -0700

    Class ending

--- w5d4/recursion.js
+++ w5d4/recursion.js
@@ -44,3 +44,49 @@ function exponent2(base, n) {
 }
 
 // console.log(exponent2(3,3))
+
+function nthFib(n) {
+  if( n <= 1) {
+    return 0
+  } else if ( n === 2) {
+    return 1
+  } else {
+    return nthFib(n - 1) + nthFib(n - 2)
+  }
+}
+
+function fib(n){
+  if (n === 1){
+    return [0];
+  } else if (n === 2) {
+    return [0,1];
+  } else {
+    return fib(n-1).concat([nthFib(n)]);
+  }
+}
+
+// function binarySearch(array, target) {
+//   if (array.length === 0) {
+//     return null
+//   } else {
+//     var splitterIndex = Math.floor(array.length/2)
+//     var splitter = array[splitterIndex]
+//     slice_smaller = array.slice(0, splitterIndex + 1)
+//     slice_bigger = array.slice(splitterIndex + 1, array.length)
+//     console.log(slice_smaller)
+//     console.log(slice_bigger)
+//     if(splitter === target) {
+//       return splitterIndex
+//     } else if (splitter > target){
+//       return binarySearch(slice_smaller, 3);
+//     } else {
+//       if (binarySearch(slice_bigger, 3) === null) {
+//         return null;
+//       } else {
+//         return slice_smaller.length + binarySearch(slice_bigger, 3);
+//       }
+//     }
+//   }
+// }
+//
+// console.log(binarySearch([0,1,2,3,4,5], 4))
