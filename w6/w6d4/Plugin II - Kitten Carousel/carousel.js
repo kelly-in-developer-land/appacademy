342d0f94243e2703d4a3d0c01a852d5661e710c4
kelly-in-developer-land <kelly.witwickifaddegon@gmail.com>
2015-05-28 18:36:10 -0700

    Changing sliding, currently broken

--- w6/w6d4/Plugin II - Kitten Carousel/carousel.js
+++ w6/w6d4/Plugin II - Kitten Carousel/carousel.js
@@ -5,25 +5,37 @@ $.Carousel = function (el) {
   this.listen();
 };
 
-$.Carousel.prototype.slide = function (dir) {
-  $("img.left").removeClass("left");
-  $("img.right").removeClass("right");
-  var direction = (dir === -1) ? "right" : "left";
-  $("img").eq(this.activeIdx).removeClass("active").addClass(direction);
+$.Carousel.prototype.slide = function (event, dir) {
+  event.preventDefault();
+  // $("img.left").removeClass("left");
+  // $("img.right").removeClass("right");
+  var direction = (dir === 1) ? "right" : "left";
+  var oppDirection = (dir === 1) ? "left" : "right";
+
+  var $oldActiveImg = $("img").eq(this.activeIdx).addClass(direction);
+
   this.activeIdx += dir;
   if (this.activeIdx <= -1 * $("img").length || this.activeIdx >= $("img").length) {
     this.activeIdx = this.activeIdx % $("img").length;
   }
-  var $activeImg = $("img").eq(this.activeIdx);
-  $activeImg.addClass("active").addClass("left");
-  setTimeout(function () {
-    $activeImg.removeClass("left");
-  }, 0);
+
+  var $activeImg = $("img").eq(this.activeIdx).addClass("active").addClass(direction);
+
+  $oldActiveImg.one("transitioned", function (event) {
+    $oldActiveImg.removeClass("active").removeClass(direction);
+    setTimeout(function () {
+      $activeImg.removeClass(direction);
+    }, 0);
+  });
 };
 
 
 $.Carousel.prototype.listen = function () {
   var that = this;
-  $(".sliders .prev").on("click", that.slide.bind(that, -1));
-  $(".sliders .next").on("click", that.slide.bind(that, 1));
+  $(".sliders .prev").on("click", function (event) {
+    that.slide.call(that, event, -1);
+  });
+  $(".sliders .next").on("click", function (event) {
+    that.slide.call(that, event, 1);
+  });
 };
