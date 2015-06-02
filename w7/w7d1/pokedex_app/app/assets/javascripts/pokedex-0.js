4af61f1c3ce5dbca3edfe74d935d693f92c95039
App Academy Student <student@appacademy.io>
2015-06-01 17:58:30 -0700

    Finish main part of assignmentString.constantize

--- w7/w7d1/pokedex_app/app/assets/javascripts/pokedex-0.js
+++ w7/w7d1/pokedex_app/app/assets/javascripts/pokedex-0.js
@@ -3,17 +3,32 @@ window.Pokedex.Models = {};
 window.Pokedex.Collections = {};
 
 Pokedex.Models.Pokemon = Backbone.Model.extend({
-  urlRoot: "/pokemon"
+  urlRoot: "/pokemon",
+  toys: function () {
+    this._toys = this._toys ||
+      new Pokedex.Collections.PokemonToys({}, { pokemon: this });
+    return this._toys;
+  },
+  parse: function (payload) {
+    if (payload.toys !== undefined) {
+      this.toys().set(payload.toys, {parse: true});
+      delete payload.toys;
+    }
+    return payload;
+  }
 });
 
-Pokedex.Models.Toy = null; // WRITE ME IN PHASE 2
+Pokedex.Models.Toy = Backbone.Model.extend({
+});
 
 Pokedex.Collections.Pokemon = Backbone.Collection.extend({
   model: Pokedex.Models.Pokemon,
   url: "/pokemon"
 });
 
-Pokedex.Collections.PokemonToys = null; // WRITE ME IN PHASE 2
+Pokedex.Collections.PokemonToys = Backbone.Collection.extend({
+  model: Pokedex.Models.Toy
+});
 
 window.Pokedex.Test = {
   testShow: function (id) {
@@ -44,6 +59,9 @@ window.Pokedex.RootView = function ($el) {
   this.$toyDetail = this.$el.find('.toy-detail');
 
   // Click handlers go here.
+  this.$pokeList.on("click", "li", (this.selectPokemonFromList).bind(this));
+  this.$newPoke.on("submit", (this.submitPokemonForm).bind(this));
+  this.$pokeDetail.on("click", "ul.toys li", (this.selectToyFromList).bind(this));
 };
 
 $(function() {
--- w7/w7d1/pokedex_app/app/assets/javascripts/pokedex-2.js
+++ w7/w7d1/pokedex_app/app/assets/javascripts/pokedex-2.js
@@ -1,8 +1,29 @@
 Pokedex.RootView.prototype.addToyToList = function (toy) {
+  var $li = $("<li>").text("Name: " + toy.escape("name") +
+    ", Happiness: " + toy.escape("happiness") +
+    ", Price: " + toy.escape("price")
+  );
+  $li.data("id", toy.id)
+  $li.data("pokemon-id", toy.escape('pokemon_id'));
+  this.$pokeDetail.find("ul.toys").append($li);
 };
 
 Pokedex.RootView.prototype.renderToyDetail = function (toy) {
+  var $div = $("<div>").addClass("detail");
+  for (key in toy.attributes) {
+    var $li = $("<li>").text(key + ": " + toy.attributes[key]);
+    $div.append($li);
+  }
+  this.$toyDetail.append($div);
 };
 
 Pokedex.RootView.prototype.selectToyFromList = function (event) {
+  event.preventDefault();
+  var tid = $(event.currentTarget).data("id");
+
+  var pid = $(event.currentTarget).data('pokemon-id');
+
+  var pokemon = this.pokes.get(pid);
+  var toy = pokemon.toys().get(tid);
+  this.renderToyDetail(toy);
 };
