e804189e982b34c216307d1ecfdc56465c59c5cc
App Academy Student <student@appacademy.io>
2015-06-02 18:00:42 -0700

    Nearly done phase 6, class ending

--- w7/w7d2/skeleton2/app/assets/javascripts/pokedex-6.js
+++ w7/w7d2/skeleton2/app/assets/javascripts/pokedex-6.js
@@ -1,23 +1,45 @@
 Pokedex.Router = Backbone.Router.extend({
   routes: {
+    "": "pokemonIndex",
+    "pokemon/:id": "pokemonDetail"
+    "pokemon/:pokemonId/toys/:toyId": "toyDetail"
   },
 
   pokemonDetail: function (id, callback) {
+    if (!this._pokemonIndex) {
+      this.pokemonIndex(function() {
+        this.pokemonDetail(id, callback);
+      }.bind(this));
+    } else {
+      var pokemon = this._pokemon.findWhere({id: parseInt(id)});
+      new Pokedex.Views.PokemonDetail({model: pokemon, el: $(".pokemon-detail")});
+    }
   },
 
   pokemonIndex: function (callback) {
+    this._pokemonIndex = new Pokedex.Views.PokemonIndex({collection: this._pokemon});
+    this._pokemonIndex.refreshPokemon({callback: callback});
+    $("#pokedex .pokemon-list").html(this._pokemonIndex.$el);
   },
 
-  toyDetail: function (pokemonId, toyId) {
+  toyDetail: function (options) {
+    if (!this._pokemonDetail) {
+      this.pokemonDetail(function() {
+        this.pokemonDetail(id, callback);
+      }.bind(this));
+    } else {
+      var pokemon = this._pokemon.findWhere({id: parseInt(id)});
+      new Pokedex.Views.PokemonDetail({model: pokemon, el: $(".pokemon-detail")});
+    }
   },
 
   pokemonForm: function () {
   }
 });
 
-/*
+
 $(function () {
-  new Pokedex.Router();
+  var router = new Pokedex.Router();
+  router._pokemon = new Pokedex.Collections.Pokemon();
   Backbone.history.start();
 });
-*/
