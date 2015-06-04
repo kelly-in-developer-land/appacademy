3e5357a7208b281ac80c8f836a39875a7f994088
kelly-in-developer-land <kelly.witwickifaddegon@gmail.com>
2015-06-04 13:59:37 -0700

    complete entry selection

--- w7/w7d4/news_reader_app/app/assets/javascripts/models/feed.js
+++ w7/w7d4/news_reader_app/app/assets/javascripts/models/feed.js
@@ -1,17 +1,17 @@
 NewsReader.Models.Feed = Backbone.Model.extend({
-  rootUrl: "api/feeds",
+  urlRoot: "api/feeds",
 
   entries: function () {
     if (!this._entries) {
-      this._entries = new NewsReader.Collections.Entries({ feed: this });
-      // this._entries.fetch({
-        // success: function () {
-          // return this._entries;
-        // }
-      // }.bind(this));
+      this._entries = new NewsReader.Collections.Entries([], { feed: this });
+      this._entries.fetch({
+        success: function () {
+          return this._entries;
+        }.bind(this)
+      });
+    } else {
+      return this._entries;
     }
-
-    return this._entries;
   },
 
   parse: function(response) {
